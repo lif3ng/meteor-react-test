61842e2ad1f5d1e48b713fc24a61fb64db962060
lif3ng <lif3ng@icloud.com>
2018-10-17 18:04:04 +0800

    step 10 filtering data with publish and subscribe : done

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -36,9 +36,21 @@ class App extends Component {
         if(this.state.hideCompleted){
             filteredTasks = filteredTasks.filter(task => !task.checked)
         }
-        return filteredTasks.map((task)=>(
-            <Task key={task._id} task={task} />
-        ))
+        // return filteredTasks.map((task)=>(
+        //     <Task key={task._id} task={task} />
+        // ))
+        return filteredTasks.map((task) => {
+          const currentUserId = this.props.currentUser && this.props.currentUser._id;
+          const showPrivateButton = task.owner === currentUserId;
+
+          return (
+            <Task
+              key={task._id}
+              task={task}
+              showPrivateButton={showPrivateButton}
+            />
+          )
+        })
     }
     toggleHideCompleted(){
         this.setState({
@@ -85,6 +97,8 @@ class App extends Component {
 }
 
 export default withTracker(() => {
+    Meteor.subscribe('tasks');
+
     return {
         tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
         incompleteCount: Tasks.find({ checked: { $ne: true }}).count(),
--- imports/ui/Task.js
+++ imports/ui/Task.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Meteor } from 'meteor/meteor'
-import { Tasks } from '../api/tasks'
+import classnames from 'classnames'
 
 export default class Task extends Component {
     toggleChecked(){
@@ -15,9 +15,16 @@ export default class Task extends Component {
       Meteor.call('tasks.remove',this.props.task._id)
     }
 
-    render() {
-        const taskClassName = this.props.task.checked ? 'checked' : ''
+    togglePrivate() {
+      Meteor.call('task.setPrivate', this.props.task._id, ! this.props.task.private);
+    }
 
+    render() {
+        // const taskClassName = this.props.task.checked ? 'checked' : ''
+        const taskClassName = classnames({
+          checked: this.props.task.checked,
+          private: this.props.task.private
+        })
         return (
             <li className={taskClassName}>
                 <button className="delete" onClick={this.deleteThisTask.bind(this)}>
@@ -29,6 +36,13 @@ export default class Task extends Component {
                  checked={!!this.props.task.checked}
                  onClick={this.toggleChecked.bind(this)}
                 />
+              {
+                this.props.showPrivateButton ? (
+                  <button className="toggle-private" onClick={this.togglePrivate.bind(this)}>
+                    { this.props.task.private ? 'Private':'Public'}
+                  </button>
+                ): ''
+              }
                 <span className="text">
                     <strong>{this.props.task.username}</strong>:{this.props.task.text}
                 </span>
